c5908e0ff997b4110a134f33eba459a78f976b67
Doug Rosenberg <doug.rosenberg@gmail.com>
2020-10-03 21:21:40 -0500

    made everything closer to the cats exercise

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require("express");
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
